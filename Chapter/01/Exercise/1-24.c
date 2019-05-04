675cdc43b4af7b2de097fe5e7495d5561cbfdfd7
sil3ntd4d <sil3ntd4d@gmail.com>
2019-05-04 23:46:59 +0800

    Exercise 1-24

---
+++ Chapter/01/Exercise/1-24.c
@@ -0,0 +1,82 @@
+/* Exercise 1-24. Write a program to check a C program for rudimentary syntax
+ * errors like unbalanced parenthesis, brackets and braces. Don't forget about
+ * quotes, both single and double, escape sequences, and comments. (This program
+ * is hard if you do it in full generality.)
+ */
+
+/* Procedure:
+
+   Process the input per character. 
+   To check for unbalanced parenthesis, we count the total number of open
+   parentheses and the close parentheses. Compare the open and close
+   parentheses, if they are equal, they are balanced. Provided if they are
+   outside a comment.
+
+   Prior to this exercise the escape sequence that have been discussed are:
+
+   \t, \n, \b, \" and \\.
+ 
+*/
+
+#include <stdio.h>
+#define IN  1		/* inside a comment */
+#define OUT 0		/* outside a comment */
+
+main()
+{
+	int c, state, comment, paren, brace, bracket;
+	int double_q, single_q, esc_sequence;
+
+	comment = paren = brace = bracket = 0;
+	double_q = single_q = esc_sequence = 0;
+	state = OUT;
+	while ((c = getchar()) != EOF) {
+		if (c == '/' && state == OUT) {
+			/* Check if the next character is an '*' */
+			c = getchar();
+			if (c == '*') {
+				state = IN;
+				++comment;
+			}
+		} else if (c == '*' && state == IN) {
+			/* Check if the next character is a '/' */
+			c = getchar();
+			if (c == '/') {
+				state = OUT;
+				--comment;
+			}
+		} else if (state == OUT) {
+			if (c == '(')
+				++paren;
+			else if (c == ')')
+				--paren;
+			else if (c == '{')
+				++bracket;
+			else if (c == '}')
+				--bracket;
+			else if (c == '[')
+				++bracket;
+			else if (c == ']')
+				--bracket;
+			else if (c == 0x22) 	/* ascii value of double quote */
+				++double_q;
+			else if (c == 0x27)		/* ascii value of single quote */ 
+				++single_q;
+			else if (c == 0x5c) { 	/* condition fot the escape sequences */
+				/* Get the next character */
+				c = getchar();
+				if (c != 'n' && c != 't' && c != 'b' && c != 0x22 && c != 0x5c)
+					++esc_sequence;
+			}
+		}
+
+	}
+
+	if (comment != 0 || paren != 0 || bracket != 0 || brace != 0)
+		printf("Syntax Error !!!\n");
+	if (double_q % 2 != 0 || single_q % 2 != 0 && esc_sequence != 0)
+		printf("Syntax Error !!!\n");
+
+
+	return 0;
+}
