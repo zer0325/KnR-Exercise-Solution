15d84a461631a20e82a4600ebe88f376594767a2
sil3ntd4d <sil3ntd4d@gmail.com>
2020-01-30 14:04:01 +0800

    Exercise 5-11

---
+++ Chapter/05/Exercise/5-11.c
@@ -0,0 +1,5 @@
+/* Exercise 5-11. Modify the programs entab and detab (written as excercise in
+ * Chapter 1) to accept a list of tab stoos as arguments. Use the default tab
+ * settings if there are no arguments.
+ */
+
